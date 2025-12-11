a6cc67098120c2fe4032c5c9570369bf39ce3392
rezaanur <rezanur2610@gmail.com>
2025-12-11 11:05:15 +0700

    second commit

--- quasar.config.js
+++ quasar.config.js
@@ -47,7 +47,7 @@ export default defineConfig((ctx) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
-      // publicPath: '/',
+      publicPath: '/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
